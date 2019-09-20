elastic/elasticsearch

support proper ValuesSourceConfig resolution of GEO and GEO_SHAPE. (#45317)
* support proper ValuesSourceConfig resolution of GEO and GEO_SHAPE.

This change includes introduction of
Geo and Geo-Shape ValueSourceType and ValueType.

- include missing value resolution
- include ability for aggregations to support either geo_point or geo_shape

* add docs and rename IndexGeoFieldData to IndexGeometryFieldData

server/src/main/java/org/elasticsearch/index/fielddata/MultiGeoValues.java
@@ -18,10 +18,16 @@
  */
 package org.elasticsearch.index.fielddata;
 
+import org.elasticsearch.common.geo.Extent;
 import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.geo.GeometryTreeReader;
+import org.elasticsearch.common.geo.GeometryTreeWriter;
+import org.elasticsearch.geo.geometry.Geometry;
+import org.elasticsearch.geo.utils.GeographyValidator;
+import org.elasticsearch.geo.utils.WellKnownText;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * A stateful lightweight per document set of geo values.
@@ -95,14 +101,19 @@ public abstract class MultiGeoValues {
     }
 
     public static class GeoShapeValue implements GeoValue {
+        private static final WellKnownText MISSING_GEOMETRY_PARSER = new WellKnownText(true, new GeographyValidator(true));
+
         private final GeometryTreeReader reader;
+        private final Extent extent;
 
         public GeoShapeValue(GeometryTreeReader reader) throws IOException {
             this.reader = reader;
+            this.extent = reader.getExtent();
         }
 
-        public GeoShapeValue() {
+        public GeoShapeValue(Extent extent) {
             this.reader = null;
+            this.extent = extent;
         }
 
         @Override
@@ -113,6 +124,16 @@ public abstract class MultiGeoValues {
         @Override
         public double lon() {
             throw new UnsupportedOperationException("centroid of GeoShape is not defined");
+        }
+
+        public static GeoShapeValue missing(String missing) {
+            try {
+                Geometry geometry = MISSING_GEOMETRY_PARSER.fromWKT(missing);
+                GeometryTreeWriter writer = new GeometryTreeWriter(geometry);
+                return new GeoShapeValue(writer.extent());
+            } catch (IOException | ParseException e) {
+                throw new IllegalArgumentException("Can't apply missing value [" + missing + "]", e);
+            }
         }
     }
 

boboub/LOG8371

server/src/main/java/org/elasticsearch/index/fielddata/MultiGeoValues.java
@@ -18,10 +18,16 @@
  */
 package org.elasticsearch.index.fielddata;
 
+import org.elasticsearch.common.geo.Extent;
 import org.elasticsearch.common.geo.GeoPoint;
 import org.elasticsearch.common.geo.GeometryTreeReader;
+import org.elasticsearch.common.geo.GeometryTreeWriter;
+import org.elasticsearch.geometry.Geometry;
+import org.elasticsearch.geometry.utils.GeographyValidator;
+import org.elasticsearch.geometry.utils.WellKnownText;
 
 import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * A stateful lightweight per document set of geo values.
@@ -95,14 +101,19 @@ public abstract class MultiGeoValues {
     }
 
     public static class GeoShapeValue implements GeoValue {
+        private static final WellKnownText MISSING_GEOMETRY_PARSER = new WellKnownText(true, new GeographyValidator(true));
+
         private final GeometryTreeReader reader;
+        private final Extent extent;
 
         public GeoShapeValue(GeometryTreeReader reader) throws IOException {
             this.reader = reader;
+            this.extent = reader.getExtent();
         }
 
-        public GeoShapeValue() {
+        public GeoShapeValue(Extent extent) {
             this.reader = null;
+            this.extent = extent;
         }
 
         @Override
@@ -113,6 +124,16 @@ public abstract class MultiGeoValues {
         @Override
         public double lon() {
             throw new UnsupportedOperationException("centroid of GeoShape is not defined");
+        }
+
+        public static GeoShapeValue missing(String missing) {
+            try {
+                Geometry geometry = MISSING_GEOMETRY_PARSER.fromWKT(missing);
+                GeometryTreeWriter writer = new GeometryTreeWriter(geometry);
+                return new GeoShapeValue(writer.extent());
+            } catch (IOException | ParseException e) {
+                throw new IllegalArgumentException("Can't apply missing value [" + missing + "]", e);
+            }
         }
     }
 