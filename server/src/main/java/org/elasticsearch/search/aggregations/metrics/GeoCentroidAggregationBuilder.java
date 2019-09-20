elastic/elasticsearch

Add geo_shape support to geo_centroid aggregation (#46299)
* Add geo_shape support to geo_centroid aggregation

This commit annotates geometries with centroid information
at index-time. Then each shapes centroid is used as input to
the geo_centroid aggregator so that it effectively calculates
the centroid of centroids of shapes within each bucket.

The centroid of each geo_shape field is calculated as the
centroid of all the points of all the shapes in the geometry
collection.

The centroid of each shape is calculated the same way geo_centroid
was -- using a kahan-summation to reduce precision loss.

* add centroid test

server/src/main/java/org/elasticsearch/search/aggregations/metrics/GeoCentroidAggregationBuilder.java
@@ -39,13 +39,13 @@ import java.io.IOException;
 import java.util.Map;
 
 public class GeoCentroidAggregationBuilder
-        extends ValuesSourceAggregationBuilder.LeafOnly<ValuesSource.GeoPoint, GeoCentroidAggregationBuilder> {
+        extends ValuesSourceAggregationBuilder.LeafOnly<ValuesSource.Geo, GeoCentroidAggregationBuilder> {
     public static final String NAME = "geo_centroid";
 
     private static final ObjectParser<GeoCentroidAggregationBuilder, Void> PARSER;
     static {
         PARSER = new ObjectParser<>(GeoCentroidAggregationBuilder.NAME);
-        ValuesSourceParserHelper.declareGeoPointFields(PARSER, true, false);
+        ValuesSourceParserHelper.declareGeoFields(PARSER, true, false);
     }
 
     public static AggregationBuilder parse(String aggregationName, XContentParser parser) throws IOException {
@@ -78,7 +78,7 @@ public class GeoCentroidAggregationBuilder
     }
 
     @Override
-    protected GeoCentroidAggregatorFactory innerBuild(SearchContext context, ValuesSourceConfig<ValuesSource.GeoPoint> config,
+    protected GeoCentroidAggregatorFactory innerBuild(SearchContext context, ValuesSourceConfig<ValuesSource.Geo> config,
             AggregatorFactory parent, Builder subFactoriesBuilder) throws IOException {
         return new GeoCentroidAggregatorFactory(name, config, context, parent, subFactoriesBuilder, metaData);
     }

boboub/LOG8371

server/src/main/java/org/elasticsearch/search/aggregations/metrics/GeoCentroidAggregationBuilder.java
@@ -39,13 +39,13 @@ import java.io.IOException;
 import java.util.Map;
 
 public class GeoCentroidAggregationBuilder
-        extends ValuesSourceAggregationBuilder.LeafOnly<ValuesSource.GeoPoint, GeoCentroidAggregationBuilder> {
+        extends ValuesSourceAggregationBuilder.LeafOnly<ValuesSource.Geo, GeoCentroidAggregationBuilder> {
     public static final String NAME = "geo_centroid";
 
     private static final ObjectParser<GeoCentroidAggregationBuilder, Void> PARSER;
     static {
         PARSER = new ObjectParser<>(GeoCentroidAggregationBuilder.NAME);
-        ValuesSourceParserHelper.declareGeoPointFields(PARSER, true, false);
+        ValuesSourceParserHelper.declareGeoFields(PARSER, true, false);
     }
 
     public static AggregationBuilder parse(String aggregationName, XContentParser parser) throws IOException {
@@ -78,7 +78,7 @@ public class GeoCentroidAggregationBuilder
     }
 
     @Override
-    protected GeoCentroidAggregatorFactory innerBuild(QueryShardContext queryShardContext, ValuesSourceConfig<ValuesSource.GeoPoint> config,
+    protected GeoCentroidAggregatorFactory innerBuild(QueryShardContext queryShardContext, ValuesSourceConfig<ValuesSource.Geo> config,
                                                       AggregatorFactory parent, Builder subFactoriesBuilder) throws IOException {
         return new GeoCentroidAggregatorFactory(name, config, queryShardContext, parent, subFactoriesBuilder, metaData);
     }